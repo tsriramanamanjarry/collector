3d9724ff5b1d7afb5479a1715398526f22a56626
tsriramanamanjarry <143001940+tsriramanamanjarry@users.noreply.github.com>
2024-10-04 09:35:28 +0530

    Create collector.java

---
+++ collector.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Example {
+
+	public static void main(String[] args) {
+		ArrayList<Object> c = new ArrayList<Object>();
+		  
+ c.add(23);
+ c.add("hai");
+ System.out.println(c);
+	}
+
+}
